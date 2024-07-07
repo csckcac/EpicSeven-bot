30c5dd84123b39c825d0af2822f654bd76a7ee37
Lyuuwu <kevin42572445@gmail.com>
2024-07-07 21:39:37 +0800

    加 讀/存 json 至class.py

--- EpicSeven/core/classes.py
+++ EpicSeven/core/classes.py
@@ -1,5 +1,15 @@
 from discord.ext import commands
+import json
+import aiofiles
 
 class Cog_Extension(commands.Cog) :
     def __init__(self, bot) :
         self.bot = bot
+        
+    async def load_json(self, file_path) :
+        async with aiofiles.open(file_path, mode='r', encoding='utf-8') as f :
+            return json.loads(await f.read())
+    
+    async def save_json(self, file_path, data) :
+        async with aiofiles.open(file_path, mode='w', encoding="utf-8") as f :
+            await f.write(json.dumps(data, ensure_ascii=False, indent=4))
