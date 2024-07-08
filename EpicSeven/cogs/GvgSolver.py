f36d034348c53d0c6209f5c594c6c377369769d6
Lyuuwu <kevin42572445@gmail.com>
2024-07-08 09:03:35 +0800

    GVG進攻解陣

--- EpicSeven/cogs/GvgSolver.py
+++ EpicSeven/cogs/GvgSolver.py
@@ -110,13 +110,14 @@ class GvgSolver(Cog_Extension) :
         except Exception as e :
             await interaction.followup.send(e)
     
+    '''
     @bot.tree.command(
         name = "solve_gvg_helper",
         description = "solve_gvg 指令使用說明",
         guild = discord.Object(id=setdata["Discord-Server-Id"]["main"])
     )
     async def help_(self, interaction) :
-        await interaction.response.send_message("請去看 <#1256273173414940743>")
+        await interaction.response.send_message("請去看 <#1256273173414940743>")'''
     
     
     @bot.tree.command(
@@ -217,6 +218,49 @@ class GvgSolver(Cog_Extension) :
             await interaction.followup.send(f"完成刪除!!")
         except Exception as e :
             await interaction.followup.send(f"發生 {e} 的錯誤!\n請再試一次 >.<")
+            
+    @bot.tree.command(
+        name="gvg_helper",
+        description="GVG進攻解陣指令 - 使用說明"
+    )
+    async def gvg_helper(self, interaction) :
+        try :
+            embed1 = discord.Embed(title="GVG進攻陣容指令 - 使用說明")
+            
+            embed2 = discord.Embed(title="1. 在<#1256260728894001193> 輸入 '/' (不用輸入')")
+            
+            embed3 = discord.Embed(title='2. 點選左側 "第七史詩" 圖示')
+            embed3.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259512664686854214/2024-07-07_221150.png?ex=668bf413&is=668aa293&hm=3d37ed7e76db4318d66ccbe2bcb73d7d39645c9ac38f4f22cb8144a1c34e54a6&=&format=webp&quality=lossless")
+            
+            embed4 = discord.Embed(title="3. 點選右方 /solve_gvg (也可以直接輸入/solve_gvg 就可以跳過2.)")
+            embed4.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259513240116006932/2024-07-07_221505.png?ex=668bf49d&is=668aa31d&hm=ed2f327b0a85c4f4bdd2a9268c3a958a04e86bb7b7a3fb2b2753aabda65736dc&=&format=webp&quality=lossless")
+            
+            embed5 = discord.Embed(title="4. 輸入英雄名稱(支援遊戲內的名稱、玩家間的簡稱、英文名稱) 均不用輸入完整", description="但是因為discord API 的限制，選項至多只能顯示25個，因此建議輸入完整一點，例如: 光賽(O) 光(X)")
+            embed5.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259513459683622952/image.png?ex=668bf4d1&is=668aa351&hm=ecfedd9ab6c7349cca23b5c68297691ddeedf0641647e1cb11eb181ae3f70df3&=&format=webp&quality=lossless")
+            
+            embed6 = discord.Embed(title="5. 重複 4. 直到選完三個英雄")
+            embed6.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259517164470145114/image.png?ex=668bf844&is=668aa6c4&hm=f214d4902a43336cd6b0d772c26188ce625596518ba783e6fb04221717cf575a&=&format=webp&quality=lossless&width=550&height=33")
+            
+            embed7 = discord.Embed(title="6. 按下鍵盤Enter鍵 或 手機的送出鍵 得到解陣陣容")
+            embed7.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259517263531479140/image.png?ex=668bf85c&is=668aa6dc&hm=90694c256d238009a140ed3661872cfdc2ef6e8ded5dc551016776301e87b1d6&=&format=webp&quality=lossless&width=486&height=671")
+            
+            embed8 = discord.Embed(title="7-1. 錯誤訊息: 重複選擇")
+            embed8.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259675328125603840/2024-07-07_225056.png?ex=668c8b91&is=668b3a11&hm=f1d9b80ea040ef91df0416bf50b7b086fe56b1254a00a238cc304245a83dd32b&=&format=webp&quality=lossless")
+            
+            embed9 = discord.Embed(title="7-2. 錯誤訊息: 直接輸入(沒有選擇)")
+            embed9.set_image(url="https://media.discordapp.net/attachments/1259512583623278673/1259675328318804070/2024-07-07_225214.png?ex=668c8b91&is=668b3a11&hm=5e30c8f751d08860194a84cc3283db5118f08610be540ab3031afb158ee37fb0&=&format=webp&quality=lossless")
+            
+            embed10 = discord.Embed(title="8. 注意事項")
+            embed10.add_field(name="a. 不能選擇重複的角色，bot會回傳錯誤訊息。", value="", inline=False)
+            embed10.add_field(name="b. 三個選項都要選，不能空著。", value="", inline=False)
+            embed10.add_field(name="c. 僅支援繁體中文與英文", value="", inline=False)
+            embed10.add_field(name="d. 不支援同音不同字的搜尋，如: 光賽納(X) 光賽娜(O)，請特別注意。", value="", inline=False)
+            embed10.add_field(name="e. 這個指令(/solve_gvg) 只能在 <#1256260728894001193> 使用。", value="", inline=False)
+            embed10.add_field(name="f. 此bot是基於此網頁 https://fribbels.github.io/e7/gw-meta.html", value="", inline=False)
+            
+            await interaction.response.send_message(embeds=(embed1, embed2, embed3, embed4, embed5, embed6, embed7, embed8, embed9, embed10))
+        except Exception as e :
+            await interaction.response.send_message(e)
     
     
 async def setup(bot) :
