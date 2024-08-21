8852ed3c3359eefa006518b82b7e47a871473a48
Lyuuwu <kevin42572445@gmail.com>
2024-08-21 21:19:29 +0800

    小玩具 簡易抽卡系統

--- EpicSeven/cogs/Banner.py
+++ EpicSeven/cogs/Banner.py
@@ -12,6 +12,7 @@ class Banner(Cog_Extension) :
         super().__init__(*args, **kwargs)
     
     @app_commands.command(name="summon", description="抽卡! 誰出彩誰是狗")
+    @app_commands.guilds(setdata["Discord-Server-Id"]["main"], setdata["Discord-Server-Id"]["test"])
     async def summon(self, interaction : discord.Interaction) :
         r_cnt = 0
         res = []
@@ -20,7 +21,7 @@ class Banner(Cog_Extension) :
         for _ in range(10) :
             ran_int = random.randint(1, 1000)
             
-            if ran_int <= 50 :
+            if ran_int <= 20 :
                 res.append("ssr")
             elif ran_int <= 200 :
                 res.append("sr")
@@ -38,6 +39,37 @@ class Banner(Cog_Extension) :
                 output += "<:sr:1009113698763296858>"
             else :
                 output += "<:ssr:1205865912167571506>"
+
+        await interaction.response.send_message(output)
+        
+    @app_commands.command(name="epic_summon", description="E7抽卡! 誰出彩誰是狗")
+    @app_commands.guilds(setdata["Discord-Server-Id"]["main"], setdata["Discord-Server-Id"]["test"])
+    async def epic_summon(self, interaction : discord.Interaction) :
+        r_cnt = 0
+        res = []
+        output = ""
+        
+        for _ in range(10) :
+            ran_int = random.randint(1, 1000)
+            
+            if ran_int <= 20 :
+                res.append("ssr")
+            elif ran_int <= 200 :
+                res.append("sr")
+            else :
+                res.append("r")
+                r_cnt += 1
+        
+        if r_cnt == 10 :
+            res[9] = "sr"
+            
+        for r in res :
+            if r == "r" :
+                output += "<:r:1255810106126368768>"
+            elif r == "sr" :
+                output += "<:sr:1256157022836097085>"
+            else :
+                output += "<:ssr:1256177377680035860>"
 
         await interaction.response.send_message(output)
 
